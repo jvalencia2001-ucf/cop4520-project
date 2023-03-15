482f95138781b442d232f5cca7b0f46cd18470fd
jvalencia2001-ucf <jvalencia.ucf@gmail.com>
2023-03-15 15:54:22 -0400

    Coded the dot product method.

---
+++ source/matrixmult.cpp
@@ -0,0 +1,48 @@
+#include <iostream> 
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> dot_matrix_mult(vector<vector<int>>& matA, vector<vector<int>>& matB) {
+    int rowA = matA.size();
+    int colA = matA[0].size();
+    int rowB = matB.size();
+    int colB = matB[0].size();
+
+    cout << rowA << " " << colA << " " << rowB << " " << colB << endl; 
+     vector<vector<int>> result(rowA, vector<int>(colB, 0));
+
+    for(int i = 0; i < rowA; i++)
+    {
+        for(int j = 0; j < colB; j++)
+        {
+            for(int k = 0; k < colA; k++)
+            {
+                result[i][j] += matA[i][k] * matB[k][j];
+            }
+        }
+    }
+
+    return result;
+}
+
+
+
+int main(){
+    vector<vector<int>> mA = {{1, 2, 3, 4},{5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    vector<vector<int>> mB = {{17, 18, 19, 20},{21, 22, 23, 24}, {25, 26, 27, 28}, {29, 30, 31, 32}};
+    vector<vector<int>> res = dot_matrix_mult(mA, mB);
+
+    for(int i = 0; i < res.size(); i++)
+    {
+        for(int j = 0; j < res[0].size(); j++)
+        {
+            cout << res[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+
+}
